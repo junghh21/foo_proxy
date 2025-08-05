05bcb269d6125cea367da5a897dfcd8469db27ec
junghh21 <junghh21@gmail.com>
2025-08-06 00:25:39 +0900

    sd

--- taskman.py
+++ taskman.py
@@ -14,7 +14,46 @@ from client import AioStratumClient
 
 async def worker1(id, queue, job, input_swap, start_nonce):
 	url = "https://127.0.0.1:8001/params"
+	url = "https://foo-job.onrender.com/params"
+
+	bin = input_swap.hex()
+	no = 	os.urandom(4).hex()#"40000000"#
+	mask = 	"00050000"
+
+	cert_file = 'cert.pem'
+	key_file = 'key.pem'
+	ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+	ssl_context.check_hostname = False
+	ssl_context.verify_mode = ssl.CERT_NONE
+	ssl_context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
+	#print(f"Successfully loaded certificate from '{cert_file}' and key from '{key_file}'.")
 	
+	try:
+		connector = aiohttp.TCPConnector(ssl=ssl_context)
+		async with aiohttp.ClientSession(connector=connector) as session:
+			async with session.post(url, json={'bin': f"{bin}", 'no': f"{no}", 'mask': f"{mask}"}) as response:
+				async for line in response.content:
+					line = line.decode('utf-8')
+					data = json.loads(line)
+					#print(data)
+					if data['result'] == "True":
+						await queue.put({
+							'type': 2,
+							'job_id': job['job_id'],
+							'extranonce2': job['extranonce2'],
+							'ntime': job['ntime'], # ntime is typically part of the job
+							'nonce': data['no'] # Nonce must be a hex string for submission
+						})
+					else:
+						print(f"{id} ...")
+	except asyncio.CancelledError:
+		print(f"Worker {id} cancelled.")
+	except:
+		pass
+
+async def worker2(id, queue, job, input_swap, start_nonce):
+	url = "https://foo-render-hh.onrender.com/params"
+
 	bin = input_swap.hex()
 	no = 	os.urandom(4).hex()#"40000000"#
 	mask = 	"00050000"
@@ -67,7 +106,7 @@ async def task_manager_loop (client: AioStratumClient):
 	Manages mining tasks, dispatching work to a process pool executor.
 	This keeps the main asyncio event loop non-blocked.
 	"""	
-	num_workers = 4#os.cpu_count() or 4  # Default to 4 if cpu_count fails
+	num_workers = 8#os.cpu_count() or 4  # Default to 4 if cpu_count fails
 	while True:		
 		try:
 			#await asyncio.sleep(0)
@@ -120,6 +159,8 @@ async def task_manager_loop (client: AioStratumClient):
 							end_nonce = nonce_space  # Ensure the last worker covers the full range
 					task = loop.create_task	(worker1(i, client.job_queue, job, input_swap, start_nonce))
 					mining_tasks.append(task)				
+					task = loop.create_task	(worker2(i+4, client.job_queue, job, input_swap, start_nonce))
+					mining_tasks.append(task)		
 		except asyncio.CancelledError:
 			break # The manager task itself was cancelled
 		except asyncio.TimeoutError:
