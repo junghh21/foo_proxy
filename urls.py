ae182f6fb992d33a552f3abfa594ad1e4e064ccf
junghh21 <junghh21@gmail.com>
2025-08-08 11:09:21 +0900

    ads

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 urls_b = [
   "https://localhost:10000/params",
   
-  "https://compassionate-prosperity-production-d266.up.railway.app/params2",
+  "https://compassionate-prosperity-production-d266.up.railway.app/params",
   
   "https://168.107.10.52:10000/params",
   "https://138.2.119.214:10000/params",
