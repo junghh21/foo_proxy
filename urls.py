6c412344277fdf67c59ced53c7622de1b7ef6b06
junghh21 <junghh21@gmail.com>
2025-08-08 11:06:32 +0900

    adda

--- urls.py
+++ urls.py
@@ -1,17 +1,7 @@
 urls_b = [
   "https://localhost:10000/params",
   
-  "https://impartial-art-production.up.railway.app/params",
-  "https://impartial-art-copy-production.up.railway.app/params",
-  "https://impartial-art-copy-1-production.up.railway.app/params",
-  "https://impartial-art-copy-2-production.up.railway.app/params",
-  "https://impartial-art-copy-3-production.up.railway.app/params",
-
-  "https://radiant-respect-production.up.railway.app/params",
-  
-  "https://foojob-production.up.railway.app/params",
-  
-  "https://lavish-balance-production-845e.up.railway.app/params",
+  "https://compassionate-prosperity-production-d266.up.railway.app/params2",
   
   "https://168.107.10.52:10000/params",
   "https://138.2.119.214:10000/params",
@@ -22,15 +12,5 @@ urls_b = [
 urls_m = [
   "https://localhost:10000/params2",
   
-  "https://impartial-art-production.up.railway.app/params2",
-  "https://impartial-art-copy-production.up.railway.app/params2",
-  "https://impartial-art-copy-1-production.up.railway.app/params2",
-  "https://impartial-art-copy-2-production.up.railway.app/params2",
-  "https://impartial-art-copy-3-production.up.railway.app/params2",
-    
-  "https://radiant-respect-production.up.railway.app/params2",
-  
-  "https://foojob-production.up.railway.app/params2",
-  
-  "https://lavish-balance-production-845e.up.railway.app/params2",
+  "https://compassionate-prosperity-production-d266.up.railway.app/params2",
 ]
