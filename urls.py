1bb144dc75dbb2e6f967fa471917ba8256e934cd
junghh21 <junghh21@gmail.com>
2025-08-09 22:56:01 +0900

    asdas

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 urls_b = [
   #"https://localhost:10000/params",
   
-  "https://foo-job.vercel.app/params",
-  "https://silent-wave-77.vercel.app/params",
+  #"https://foo-job.vercel.app/params",
+  #"https://silent-wave-77.vercel.app/params",
   
   "https://compassionate-prosperity-production-d266.up.railway.app/params",
   
@@ -17,8 +17,8 @@ urls_m = [
   
   #"http://localhost:3000/params2",
   
-  "https://foo-job.vercel.app/params2",
-  "https://silent-wave-77.vercel.app/params2",
+  #"https://foo-job.vercel.app/params2",
+  #"https://silent-wave-77.vercel.app/params2",
   
   #"https://a9ec5477-6c1a-4fc4-85ab-cfb81296af53-00-2wxzg3lm49vuu.sisko.replit.dev/params2",
 
